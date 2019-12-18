941e3ba9d8555bae412a0e3ec745757f03b24504
simonsoubeyrand <simonsoubeyrand@gmail.com>
2019-12-18 11:44:07 +0000

    created script to refer and index specific trial's stimuli; added some more explanation for the experiment; wait for the new gender_DNN_input.mat, I just updated it

--- data_input_dfm/import_features_xp.py
+++ data_input_dfm/import_features_xp.py
@@ -1,6 +1,8 @@
-from scipy.io import loadmat
 import os.path as op
-import h5py as h5py
+from scipy.io import loadmat
+import h5py
+import numpy as np
+from show_trial_experiment import show_trial
 
 # put the task here (either gender, or emotion). Only gender for now
 task_string='gender'
@@ -14,7 +16,7 @@ mat_xp_labels = loadmat(op.join(base_dir, f'{task_string}_INPUT_MAT_DNN.mat'))
 # (subjects x features x labelstype) tensor containing 1) accuracy and 2) actual responses for presented face
 human_classification=mat_xp_labels['human_classification_experiment'] 
 
-# (subjects x trials x presence/absence) tensor containing presence or absence of features at a given trial
+# (subjects x trials x features presence/absence) tensor containing presence or absence of features at a given trial
 presence_feats_experiment=mat_xp_labels['presence_feats_experiment'] 
 
 # (subjects x trials x stimulus presented) tensor indicating which stimulus was presented 
@@ -23,4 +25,30 @@ which_stim_experiment=mat_xp_labels['which_stim_experiment']
 # a (stimuli x features x pixels) tensor containing the actual images of gabors.
 mat_img_gabors = h5py.File(op.join(base_dir,'Gabors_vectorized_images.mat'),'r')
 
-gabors_images=mat_img_gabors['gabor_vectorized_images']
+ # un exemple de comment plotter the ith stimulus for participant n along with the response that participant made 
+gabors_images=mat_img_gabors.get('gabor_vectorized_images')
+gabors_images=np.array(gabors_images)
+
+
+subject=101 # arbitrary subject to show
+trial=2197  # trial to show
+
+
+correct_incorrect=["incorrect","correct"] # labels for accurate/inaccurate responses 
+acc_trial=human_classification[subject,trial,0] # 1st dim for accuracy / 1 == accurate
+
+responses=["male","female"] # label for responses
+response_trial=human_classification[subject,trial,1] # 2nd dim for actual responses, also in logical
+
+
+human_resp=correct_incorrect[acc_trial]  # one can change this for responses if prefered
+
+
+#this is defined in show_trial_experiment.py ; the script is in the same folder
+show_trial(subject, 
+               trial,
+               human_resp,
+               which_stim_experiment,
+               presence_feats_experiment,
+               gabors_images)
+
---
+++ data_input_dfm/show_trial_experiment.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def show_trial(subject,
+               trial,
+               human_resp,
+               which_stim_experiment,
+               presence_feats_experiment,
+               gabors_images):
+    
+    that_stim=int(which_stim_experiment[subject,trial])-1 # gets stimulus, -1 changes matlab indexes to python indexes
+            
+    # choose the specific trial, transform in boolean
+    these_gabors=presence_feats_experiment[subject,trial,:]==1
+
+    # get all the Gabors images that we presented at this trial, for that stimulus
+    stim_ex=gabors_images[that_stim,these_gabors,:]
+
+    stim=np.sum(stim_ex,0)# sum all Gabs images along first dimension
+    stim=np.reshape(stim,[250,250]) #reshape the vectorized image to actual 250x250 image
+    stim=np.array(stim).T # transpose required from matlab to python indexing
+    
+    plt.imshow(stim, cmap=plt.cm.gray)
+    plt.axis('off')
+    plt.title(human_resp)
+    plt.show()
